0d1700d63f84fd788c300ea02135be436dd57c8a
figol <figolliu@gmail.com>
2014-07-08 14:50:10 +0800

    update

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Percentage Widget For OpenERP 7.0
 --------------------------------------------------------
 以百分比分的形式显示数字<br />
 
+<pre>
+
 安装方法:
 1. download source code；
 2. update module list；
@@ -20,3 +22,5 @@ Percentage Widget For OpenERP 7.0
 效果：
 margin: 1.1  ->  110%
 margin: 0.2  ->  20%
+
+</pre>
---
+++ web_percentage_widget/static/src/js/percentage_widget.js
@@ -0,0 +1,63 @@
+/*
+##############################################################################
+#
+#    percentage widget for openerp 7.0
+#    Copyright 2014 figol <figolliu@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+*/
+openerp.web_percentage_widget = function(instance) {
+    var _t = instance.web._t,
+        _lt = instance.web._lt;
+    var QWeb = instance.web.qweb;
+
+    // form view
+    instance.web.form.widgets.add('percentage', 'instance.web_percentage_widget.PercentageWidget');
+    instance.web_percentage_widget.PercentageWidget = instance.web.form.FieldFloat.extend({
+        display_name: _lt('PercentageWidget'),
+        template: "PercentageWidget",
+        render_value: function() {
+            if (!this.get("effective_readonly")) {
+                this._super();
+            } else {
+                var _value = parseFloat(this.get('value'));
+                if (isNaN(_value)) {
+                    this.$el.find(".percentage_filed").text();
+                }
+                else{
+                    this.$el.find(".percentage_filed").text((_value*100).toFixed(2) + '%');
+                }
+            }
+        }
+    });
+
+    // list view
+    instance.web.list.columns.add('field.percentage', 'instance.web.list.Percentage');
+    instance.web.list.Percentage = instance.web.list.Column.extend({
+        /**
+         * Return a percentage format value
+         *
+         * @private
+         */
+        _format: function (row_data, options) {
+            var _value = row_data[this.id].value;
+            if (isNaN(_value)) {
+                return null;
+            }
+            return (_value*100).toFixed(2) + '%';
+        }
+    });
+};
